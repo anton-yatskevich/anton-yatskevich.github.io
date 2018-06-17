95324282b1d48ed9e9ebfcf2cef46097df312115
anton-yatskevich <anton_yatskevich@mail.ru>
2018-06-17 22:27:04 +0300

    remove all

--- src/js/Tasks/bundleTasks.js
+++
@@ -1,5 +0,0 @@
-export { Listening } from "./ListeningTask";
-export { CompoundWords } from "./CompoundWordsTask";
-export { MathTask } from "./MathTask";
-export { SortableLetters } from "./SortableLettersTask";
-export { Translation } from "./TranslationTask";
